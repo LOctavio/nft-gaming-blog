ef9eb0c2ca61abb8adacac0d9437517936e2eff9
LOctavio <luis_ram1997@hotmail.com>
2022-01-17 13:12:52 -0600

    Fix identation

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    def update_likes_counter
-        post.update(likes_counter: posts.likes.count)
-    end
+  def update_likes_counter
+    post.update(likes_counter: posts.likes.count)
+  end
 end
