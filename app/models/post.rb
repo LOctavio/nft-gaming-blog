5108462ed4a7d6cb4366c8d22cb985f01bb59d7f
LOctavio <luis_ram1997@hotmail.com>
2022-01-17 13:08:20 -0600

    Add aditional methods to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :comments
     has_many :likes
+
+    def most_recent_comments
+        comments.limit(5).order(created_at: :desc)
+    end
+
+    def update_posts_counter
+        author.update(posts_counter: author.posts.count)
+    end
 end
