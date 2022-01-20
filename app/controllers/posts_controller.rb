8060d08166659d32a3a0c742036c9d18adad90f7
LOctavio <luis_ram1997@hotmail.com>
2022-01-19 19:28:50 -0600

    Create post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+    @user = User.find(current_user.id)
+  end
+
+  def create
+    @user = User.find(current_user.id)
+    @post = @user.posts.new(post_params)
+
+    if @post.save
+      redirect_to @user
+    end
+  end
+
   def index
     @posts = Post.all
     @user = User.find(params[:user_id])
@@ -6,5 +20,11 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :create]
   end
+  get 'new_post', to: 'posts#new'
 end
