b997122a52caec4320b53f406204de8bf042c9e2
LOctavio <luis_ram1997@hotmail.com>
2022-01-20 15:35:16 -0600

    Add like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+    def create
+        like = Like.new(
+        author_id: current_user.id,
+        post_id: params[:post_id]
+        )
+
+        if like.save
+            redirect_to user_post_path(id: params[:post_id])
+        end
+    end
 end
