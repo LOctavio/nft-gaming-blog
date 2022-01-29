4191805114cd9af94c87dd11158b2e564463c85c
DyaryRaoof <dyary2020@gmail.com>
2022-01-29 22:50:33 +0300

    remove logging code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,35 +1,33 @@
 class Users::SessionsController < Devise::SessionsController
-  skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
+  
+    # respond_to :json
 
-  # respond_to :json
+    def new
+        respond_to do |format|
+            format.html {@user =  User.new}
+            format.json
+        end
+    end
 
-  def new
-    respond_to do |format|
-      format.html { @user = User.new }
-      format.json
+    private
+  
+    def respond_with(resource, _opts = {})
+      render json: { message: 'You are logged in.' }, status: :ok
+    end
+  
+    def respond_to_on_destroy
+      puts current_user
+      log_out_success && return if current_user
+  
+      log_out_failure
+    end
+  
+    def log_out_success
+      render json: { message: "You are logged out." }, status: :ok
+    end
+  
+    def log_out_failure
+      render json: { message: "Hmm nothing happened."}, status: :unauthorized
     end
   end
-
-  private
-
-  def respond_with(_resource, _opts = {})
-    render json: { message: 'You are logged in.' }, status: :ok
-  end
-
-  def respond_to_on_destroy
-    puts 'here'
-    puts current_user
-    puts 'here'
-    log_out_success && return if current_user
-
-    log_out_failure
-  end
-
-  def log_out_success
-    render json: { message: 'You are logged out.' }, status: :ok
-  end
-
-  def log_out_failure
-    render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
-  end
-end
